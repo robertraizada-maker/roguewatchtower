e956f23befe4e206ff3d5e0760d9e3cb3dadd46f
Robert Raizada <29420112+robertraizada-maker@users.noreply.github.com>
2026-06-29 21:05:18 +0100

    Clean static site setup

--- .open-next/server-functions/default/.next/server/chunks/ssr/app_layout_tsx_2144vk_._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[33290,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsx)("html",{lang:"en",children:(0,b.jsx)("body",{children:a})})},"metadata",0,{title:{default:"Rogue Watchtower",template:"%s | Rogue Watchtower"},description:"Discover rogue Pokémon TCG decks before they become part of the meta."}])},70864,a=>{a.n(a.i(33290))}];
-
-//# sourceMappingURL=app_layout_tsx_2144vk_._.js.map
